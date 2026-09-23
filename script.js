425377c4ab0e14d584f6a51a9323924578c36ba1
Mamillapalli Pavani <mpavani410@gmail.com>
2026-09-23 10:19:20 +0530

    learned operators

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // console.log(2*3);
-
+//variables
 var x = 51;
 var y = 61;
 var z = x + y;
@@ -11,3 +11,9 @@ var x=12;
 var y="hii";
 console.log(x+y)
 
+//Operators
+console.log(4**2);
+ var x=10;
+ var y="10";
+ console.log(x==y);
+ console.log(x===y);
