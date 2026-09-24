b28e1e355b308e9b58c1878213604e05409a9843
Mamillapalli Pavani <mpavani410@gmail.com>
2026-09-24 21:41:27 +0530

    learn with some basic buttons

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 
-console.log(document)
-console.log(document.head)
-console.log(document.body)
+// console.log(document)
+// console.log(document.head)
+// console.log(document.body)
 
-let myData=document.getElementsByTagName("h1")
-console.log(myData[0].innerText);
+// let myData=document.getElementsByTagName("h1")
+// console.log(myData[0].innerText);
 
-let display=document.getElementById("display")
-console.log(display.innerText);
+// let display=document.getElementById("display")
+// console.log(display.innerText);
 
 // let myData1 = document.getElementsByClassName("heading");
 // console.log(myData1[1].innerText); // Welcome
 
-// let count=0;
-// let display=document.getElementById("display")
+let count=0;
+let display=document.getElementById("display")
 
-// display.innerText=count
+display.innerText=count
 
-// const incCount =() => {
-//     count++
-//     display.innerText=count
-// }
+const incCount =() => {
+    count++
+    display.innerText=count
+}
 
-// const decCount =() => {
-//     if(count>0){
-//         count--
-//     }
-//     display.innerText = count
-// }
+const decCount =() => {
+    if(count>0){
+        count--
+    }
+    display.innerText = count
+}
 
 // let count=0;
 // let display=document.getElementById("display")
