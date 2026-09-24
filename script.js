c60f7403687c032009e9a36e7c3540ca1eb045b6
Mamillapalli Pavani <mpavani410@gmail.com>
2026-09-24 21:42:29 +0530

    Dom with functions

--- script.js
+++ script.js
@@ -12,41 +12,41 @@
 // let myData1 = document.getElementsByClassName("heading");
 // console.log(myData1[1].innerText); // Welcome
 
-let count=0;
-let display=document.getElementById("display")
-
-display.innerText=count
-
-const incCount =() => {
-    count++
-    display.innerText=count
-}
-
-const decCount =() => {
-    if(count>0){
-        count--
-    }
-    display.innerText = count
-}
-
 // let count=0;
 // let display=document.getElementById("display")
 
-// function showCount() {
-//     display.innerText = count;
-// }
+// display.innerText=count
 
 // const incCount =() => {
 //     count++
-//     showCount()
+//     display.innerText=count
 // }
 
 // const decCount =() => {
 //     if(count>0){
 //         count--
 //     }
-//     showCount()
+//     display.innerText = count
 // }
-// showCount()
+
+let count=0;
+let display=document.getElementById("display")
+
+function showCount() {
+    display.innerText = count;
+}
+
+const incCount =() => {
+    count++
+    showCount()
+}
+
+const decCount =() => {
+    if(count>0){
+        count--
+    }
+    showCount()
+}
+showCount()
 
 
