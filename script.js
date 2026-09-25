7a017697a86267c84facb06b7284e036c8be5cb7
Mamillapalli Pavani <mpavani410@gmail.com>
2026-09-25 08:51:49 +0530

    Updated JavaScript code

--- script.js
+++ script.js
@@ -29,32 +29,86 @@
 //  }
 
 //loops
-for(var i=0;i<=5;i++){ 
-    console.log(i);
-}
+// for(var i=0;i<=5;i++){ 
+//     console.log(i);
+// }
 
 
 //ES6
 //var
-var a=10;
-var a=20;
-console.log(a);
+// var a=10;
+// var a=20;
+// console.log(a);
 
 //let
-let a=10;
-let a=20;
-console.log(a);
+// let b=10;
+// b=20; // here can't redeclare with same value ,but perform reinitilize
+// console.log(b);
 
 //const
-const a=10;
-const a=20;
-console.log(a);
-// let-resign
-//const -re declare
+// const c=10;
+// const d=20; //can't perform re-initilization and re-declaration
+// console.log(d);
+// let-can resign,can't re-declare
+//const -can't re-assign,re-declare
 
 //functions
-// function myFun(a,b){
+// function myFun(){
 //     console.log("hii"); 
-//     return a+b;
 // }
 // myFun();
+
+// function myFun(a,b){
+//     return a+b; 
+// }
+// //myFun(2,5);
+// console.log(myFun(2,5))
+
+//normal 
+// const addition=function myFun(a,b){    
+//     return a+b; 
+// }
+// console.log(addition(2,5))
+//anonymous
+// const sub=function (a,b){    
+//     return a-b; 
+// }
+// console.log(sub(2,5))
+//Arrow func
+// const mul=(a,b) => {    
+//     return a*b; 
+// }
+// console.log(mul(2,5))
+//Arrow func using singlie line/shorter version
+// const div=(a,b) => a/b;
+// console.log(div(2,5))
+
+//Arrays
+// let arr=[101,"hi",89.0,true]
+// let arr2=[100,200]
+// arr[0]=10;
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+// let newArr=[...arr , ...arr2];
+// console.log(newArr);
+// let obj={
+//     id:101,
+//     name:"riya",
+//     age:23
+// }
+// console.log(obj);
+//console.log(obj.age);
+
+// let data=[
+//     {
+//         id:101,
+//         name:"riya"
+//     },
+//     {
+//         id:102,
+//         name:"vincent"
+//     }
+// ]
+//console.log(data[0].name);
+
